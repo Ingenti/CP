28436412f998f204bb655326b0b9be9be4056a0c
Akseli <akseli.oinaanoja@aalto.fi>
2021-05-06 22:13:41 +0300

    2

--- cp2a/cp.cc
+++ cp2a/cp.cc
@@ -44,22 +44,20 @@ void correlate(int ny, int nx, const float *data, float *result)
     }
 
 
-    for(int c = 0; c < ny-1; c++)
+    for(int c = 0; c < ny/2; c++)
     {
-        for(int i = c; i < ny-1; i++)
+        for(int i = c; i < ny/2; i++)
         {
             double s = 0;
             double ss = 0;
-            double sss = 0;
             for(int j = 0; j < nx; j++)
             {
                 ss += mat[j+c*nx]*mat[j+i*nx];
-                s += mat[j+(c+1)*nx]*mat[j+i*nx];
-                sss += mat[j+(c+1)*nx]*mat[j+(i+1)*nx];
+
+                s += mat[j+(ny-c)*nx]*mat[j+(ny-i)*nx];
             }
             result[i+c*ny] = ss/nx;
-            result[(i+1)+(c+1)*ny] = sss/nx;
-            result[i+(c+1)*ny] = s/nx;
+            result[(ny-i)+(ny-c)*ny] = s/nx;
         }   
     }
 }
