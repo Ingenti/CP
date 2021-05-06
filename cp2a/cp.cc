ae7b4e4063d65d7766d735611a3b269d35f986e2
Akseli <akseli.oinaanoja@aalto.fi>
2021-05-06 17:44:26 +0300

    2

--- cp2a/cp.cc
+++ cp2a/cp.cc
@@ -44,7 +44,7 @@ void correlate(int ny, int nx, const float *data, float *result)
     }
 
 
-    for(int c = 0; c < ny-1; c++)
+    for(int c = 0; c < ny; c++)
     {
         for(int i = c; i < ny; i=i+2)
         {
