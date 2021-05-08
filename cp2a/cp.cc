9b94d14d5371ee8027599f66e6cf67bc6f90a0d9
Akseli <akseli.oinaanoja@aalto.fi>
2021-05-08 11:32:35 +0300

    5

--- cp2a/cp.cc
+++ cp2a/cp.cc
@@ -43,20 +43,22 @@ void correlate(int ny, int nx, const float *data, float *result)
         }
     }
 
-    #pragma omp parallel for
     for(int c = 0; c < ny -1; c+=2)
     {
         for(int i = c; i < ny -1; i+=2)
         {
             double s = 0;
             double ss = 0;
+            double sss = 0;
             for(int j = 0; j < nx; j++)
             {
                 ss += mat[j+c*nx]*mat[j+i*nx];
                 s += mat[j+c*nx]*mat[j+(i+1)*nx];
+                sss += mat[j+(c+1)*nx]*mat[j+i*nx];
             }
             result[i+c*ny] = ss/nx;
             result[(i+1)+c*ny] = s/nx;
+            result[(i+1)+(c+1)*ny] = sss/nx;
         }   
     }
 }
