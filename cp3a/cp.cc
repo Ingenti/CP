8ceeab47c2ac2bcb33fad5e9a093c096b4abc3d6
Akseli <akseli.oinaanoja@aalto.fi>
2021-05-09 13:32:20 +0300

    2

--- cp3a/cp.cc
+++ cp3a/cp.cc
@@ -36,6 +36,7 @@ double sum(double4_t a)
         if(isnan(a[i])){continue;}
         sum += a[i];
     }
+    std::cout << sum << std::endl;
     return sum;
 }
 
@@ -109,7 +110,6 @@ void correlate(int ny, int nx, const float *data, float *result)
             {
                 corr(row, nvrow, nx);
                 corr(row2, nvrow, nx);
-                std::cout <<"JYSAHTI"<< sum(row2[dvec]) <<"joku" <<std::endl;
 
                 result[j+i*ny] += sum(row[dvec]*row2[dvec]);
             }
